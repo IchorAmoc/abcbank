f632f6acb460fde15bb58d9ddbc49e812bb76fdb
IchorAmoc <vildebertelsen@gmail.com>
2021-03-06 14:03:39 +0100

    New attempt at fixing db connection

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'))
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
-app.use(methodOverride('_method'))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -25,7 +25,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true
 });
 const db = mongoose.connection;
-db.on('error', error => console.error(Error));
+db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 
